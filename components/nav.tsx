fad15eb52c08f985bf7ca05560c8f762274d00a0
Bhavya Jain <76478989+bhavya-jain-364@users.noreply.github.com>
2024-11-27 14:17:43 -0500

    Increased Moon Icon size

--- components/nav.tsx
+++ components/nav.tsx
@@ -300,7 +300,20 @@ function LogInIcon(props: any) {
 
 function LogOutIcon(props: any) {
   return(
-  <svg width="27px" height="27px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" transform="matrix(1, 0, 0, 1, 0, 0)rotate(0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="currentColor" stroke-width="0.768"></g><g id="SVGRepo_iconCarrier"> <path d="M21 12L13 12" stroke="currentColor" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9" stroke="currentColor" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19" stroke="currentColor" stroke-width="1.56" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="transform rotate-0"
+  >
+    <g stroke="currentColor" strokeWidth="1.56" strokeLinecap="round" strokeLinejoin="round">
+      <path d="M21 12H13" />
+      <path d="M18 15L20.913 12.087C20.961 12.039 20.961 11.961 20.913 11.913L18 9" />
+      <path d="M16 5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19" />
+    </g>
+  </svg>
   );
 }
 function MenuIcon(props: any) {
@@ -329,8 +342,8 @@ function MoonIcon(props: any) {
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="30"
+      height="30"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
