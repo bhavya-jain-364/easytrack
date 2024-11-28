08a4a2838d37fae2854d38ba1122dde63d0940a9
Bhavya Jain <76478989+bhavya-jain-364@users.noreply.github.com>
2024-11-27 23:14:21 -0500

    Don't show Navbar before loading the website to avoid inconsistencies when user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function Home() {
   if (isLoading) {
     return (
       <>
-        <Navb />
+        {/* <Navb /> */}
         <div className="flex justify-center items-center min-h-screen">
           <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
         </div>
